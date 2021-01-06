37dc50487eadb2aab22bbf3d770815dc041a745d
hanolee <hano.dev.kr@gmail.com>
2021-01-06 09:08:52 +0900

    solidity compiler

--- DEvid_demo/scripts/send.js
+++ DEvid_demo/scripts/send.js
@@ -1,12 +1,2 @@
-if (typeof window.ethereum !== 'undefined') {
-    console.log('MetaMask is installed!');
-  }
-  
 
 
-const ethereumButton = document.querySelector('.enableEthereumButton');
-
-ethereumButton.addEventListener('click', () => {
-  //Will Start the metamask extension
-  ethereum.request({ method: 'eth_requestAccounts' });
-});
---
+++ HashFuctionPractice/hash.js
@@ -0,0 +1,5 @@
+//Ropsten, Metamask
+address = "0xeE1A30a154dAcCb1006e38eF0F3Add1F54852CA8";
+
+console.log(address.length());
+
